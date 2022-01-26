63df8b695ea052d360d6c8dd4ea6c3c9cc02a33c
@keyur8935 <keyur8935@gmail.com>
2022-01-26 17:27:24 +0530

    Inital commit

--- lib/base/apis/api_url.dart
+++ lib/base/apis/api_url.dart
@@ -4,7 +4,8 @@ class ApiUrls {
   static const String kBaseUrl = 'https://newsapi.org';
 
   // API key
-  static const String kApiKey = 'apiKey=0c4c48dd86e84a08923e2f4beda47dc8';
+  // static const String kApiKey = 'apiKey=0c4c48dd86e84a08923e2f4beda47dc8';
+  static const String kApiKey = 'apiKey=932e5054a3764748b11fa907d688cfd2';
 
   // News HeadLines Endpoint URL
   static const String kNewsSource = '/v2/top-headlines/sources?';
